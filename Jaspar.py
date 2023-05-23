a0b8d89181ef09302f3b4f2282f82ab56f0a3baf
jessica yu <jessica.yu201201@gmail.com>
2023-05-23 10:34:58 -0700

    Created Jasper class where we initialize an empty dict and then add to the dictionary using add.

--- Jaspar.py
+++ Jaspar.py
@@ -1,4 +1,8 @@
 class Jasper:
-    def __init__(self, name, matrix, threshold, length, TF):
-        matrixdict = {}
-        matrixdict[name] = [matrix, threshold, length, TF]
+    # initialize an empty dictionary
+    def __init__(self):
+        self.matrixdict = {}
+    
+    # add JASPAR elements to the dictionary
+    def add(self, name, matrix, threshold, length, TF):
+        self.matrixdict[name] = [matrix, threshold, length, TF]
