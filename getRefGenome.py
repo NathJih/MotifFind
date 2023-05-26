a67f3cfa471b2b4d8500d4203bff546350cd2f82
P3canPi3 <jih.nathan@yahoo.com>
2023-05-26 21:25:23 +0000

    updated getReference to handle file of reference seqs

--- getRefGenome.py
+++ getRefGenome.py
@@ -1,5 +1,5 @@
 def getRefGenome(chromosome):
-    RGFile = open("chromosome" + str(chromosome) + ".fna")
+    RGFile = open("ReferenceGenome/chromosome" + str(chromosome) + ".fna")
     RGLines = RGFile.readlines()
     RefGen = ""
     for i in range(1, len(RGLines)):
