da8af88514b65b9c9b38f4304f45e1f363189923
lxp pc <lxppc@MacBook-Pro-120.attlocal.net>
2023-05-25 22:43:50 -0700

    finalize jaspar preparation functions with Jaspar original and test dataset

--- Jaspar.py
+++ Jaspar.py
@@ -1,8 +1,16 @@
-class Jasper:
+import pickle
+
+class Jaspar:
     # initialize an empty dictionary
     def __init__(self):
         self.matrixdict = {}
     
     # add JASPAR elements to the dictionary
-    def add(self, name, matrix, threshold, length, TF):
-        self.matrixdict[name] = [matrix, threshold, length, TF]
+    def add(self, name, matrix, threshold, length, TF, url):
+        self.matrixdict[name] = [matrix, threshold, length, TF, url]
+
+    # write dictionary using pickle and out put a pickle file for later usage
+    def store(self):
+        file = open('Jaspar.p', 'wb')
+        pickle.dump(self.matrixdict, file)
+        file.close()
