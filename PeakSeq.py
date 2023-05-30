845caac96509e7d4f3f4b1a1be5e7c4a3efdfa9d
P3canPi3 <jih.nathan@yahoo.com>
2023-05-30 02:55:34 +0000

    finished writing peakseq; not checked

--- PeakSeq.py
+++ PeakSeq.py
@@ -4,11 +4,22 @@ if len(sys.argv != 3):
     raise Exception("Incorrect number of arguments, 3 expected.")
 
 peaks_file = sys.argv[1]
-ref_genome_file = sys.argv[2]
 
-def PeakSeq(peaks_file, ref_genome_file):
-    # open up the files and extract the lines 
+def PeakSeq(peaks_file):
+    peakseqs = []
+    # open up the file and extract the lines 
     peaks = open(peaks_file)
-    ref_genome = open(ref_genome_file)
-
-PeakSeq(peaks_file, ref_genome_file)
+    peakslines = peaks.readlines()
+    # for each peak, extracts the specified region from the reference genome
+    for pl in peakslines:
+        pl.split()
+        chrom = pl[0].split(":")
+        chromID = chrom[1]
+        ref_genome = getRefGenome(chromID)
+        start = pl[1].split(":")
+        startSite = start[1]
+        end = pl[2].split(":")
+        endSite = end[1]
+        peakseqs.append(ref_genome[startSite - 1 : endSite]
+    return peakseqs
+PeakSeq(peaks_file)
