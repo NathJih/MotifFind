69387ce36cea980c903ba465025698e14fbbe6dc
lxp pc <lxppc@MacBook-Pro-120.local>
2023-06-02 15:12:47 -0700

    debug getmotiffind

--- Motif_Find/GetRefGenome.py
+++ Motif_Find/GetRefGenome.py
@@ -1,4 +1,15 @@
 import pickle
+import sys
+
+args = sys.argv 
+instruction = "expected: python3 GetRefGenome.py <Reference Genome Fasta File> <Output File Name>"
+
+if len(args) != 3:
+    raise Exception("Incorrect number of arguments, 3 expected.\n" + instruction)
+
+refFasta = args[1]
+print(refFasta)
+output = args[2]
 
 def GetRefGenome(refFasta):
     '''
@@ -14,16 +25,16 @@ def GetRefGenome(refFasta):
     '''
      # updated GetRefGenome to combine preprocessing steps for reference genome
     RefG = {}
-    chromosome = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y"]
+    chromosome = ["1"]
+    # "2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y"]
     RGFile = open(refFasta)
     RGLines = RGFile.readlines()
     for chr in chromosome:
         key = "chromosome" + chr
-        header_lines = []
         for i in range(0, len(RGLines)):
             if chr in RGLines[i]:
-                header_lines.append[i]
-        start = header_lines[0] + 1
+                header_lines = i
+        start = header_lines + 1
         chrom = ""
         for i in range(start, len(RGLines)):
             if "chromosome" not in RGLines[i]:
@@ -32,10 +43,10 @@ def GetRefGenome(refFasta):
                 break
         RefG[key] = chrom
 
-    file = open('GRCh38p14.p', 'wb')
+    file = open(output + ".p", 'wb')
     pickle.dump(RefG, file)
     file.close()
 
     return
 
-GetRefGenome()
+GetRefGenome(refFasta)
